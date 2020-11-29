7919e8ac49e72230547f0b01803346ea3f546704
santiago <santiago_pascual_8@hotmail.com>
2020-11-29 01:37:15 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import '@babel/polyfill';
+
+const http = require('http');
+
+const requestHandler = (req, res) => {
+  res.writeHeader(200, { ContentType: 'text/plain' });
+  res.end('Hello World my friends!!!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(8080, () => {
+  console.log('Listen on http://localhost:8080');
+});
